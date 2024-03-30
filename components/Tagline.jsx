8b294e7e730aea26de7e72df6c76fc762be641ea
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-03-31 01:08:04 +0530

    fix: Tagline.jsx animation breaking.

--- components/Tagline.jsx
+++ components/Tagline.jsx
@@ -1,34 +1,44 @@
-import { React, useEffect } from 'react'; // Import useEffect hook
+import React, { useEffect, useRef } from 'react';
 import gsap from "gsap";
-import { TextPlugin } from "gsap/dist/TextPlugin"; // Import specific plugins
+import { TextPlugin } from "gsap/dist/TextPlugin";
 
 gsap.registerPlugin(TextPlugin);
 
 function Tagline() {
+    const animationRef = useRef(null); // Reference to store animation timeline
 
     useEffect(() => {
-        // Create GSAP animation on component mount
         const words = [" Simple.", " Concise.", " Perfect."]
         const cursor = gsap.to('.cursor', { opacity: 0, ease: "power2.inOut", repeat: -1 });
         let boxTL = gsap.timeline()
         boxTL.to('.box', { duration: 1, width: "36vw", delay: 0.5, ease: "power4.inOut" }).from('.f-text', { duration: 1, y: "5vh", ease: "power3.out", onComplete: () => parentTL.play() })
-        let parentTL = gsap.timeline({ repeat: -1, }).pause()
+        let parentTL = gsap.timeline({ repeat: -1 }).pause()
         words.forEach(word => {
             let childTL = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1 })
             childTL.to('.a-text', { duration: 1, text: word })
             parentTL.add(childTL)
         })
-        // Cleanup function to clear animation on component unmount
-    }); // Empty dependency array ensures the effect runs only once after initial render
+
+        animationRef.current = parentTL; // Store the animation timeline in the ref
+
+        // Pause the animation when the component is unmounted
+        return () => {
+            if (animationRef.current) {
+                animationRef.current.pause();
+            }
+        };
+    }, []); // Empty dependency array ensures the effect runs only once after initial render
 
     return (
-        <div className='flex items-center'><h1>
-        <span className='box'></span>
-        <span className='f-text'>Rewrite your Messages to make them</span>
-        <span className='a-text'></span>
-        <span className='cursor'>|</span>
-      </h1></div>
-    )
+        <div className='flex items-center'>
+            <h1>
+                <span className='box'></span>
+                <span className='f-text'>Rewrite your Messages to make them</span>
+                <span className='a-text'></span>
+                <span className='cursor'>|</span>
+            </h1>
+        </div>
+    );
 }
 
-export default Tagline
+export default Tagline;
