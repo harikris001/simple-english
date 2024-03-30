b38d23640fbd790e1fdbaa1782f9a52baf7d7188
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-03-31 00:54:16 +0530

    fix: removed NextUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-import {nextui} from "@nextui-org/react";
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,11 +5,8 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  darkMode: "class",
-  plugins: [nextui()]
 }
