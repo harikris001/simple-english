3c10a4b919514d63cba4e2289b59ab7763d0c975
vishnuxprasad <vshnprsdvs+github@gmail.com>
2024-03-31 15:01:30 +0530

    update: UI functionality implemented.

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,23 @@
+import { useState } from "react";
 import StarField from "@/components/StarField";
 import Tagline from "@/components/Tagline";
-import { useState } from "react";
 import { IoPencilOutline } from "react-icons/io5";
+import { MdClear } from "react-icons/md";
+import { BsClipboard2Fill } from "react-icons/bs";
 
 export default function Home() {
   const [inputText, setInputText] = useState("");
   const [apiOutput, setApiOutput] = useState("");
   const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false); // State to track loading state of button
+  const [isLoading, setIsLoading] = useState(false);
+  const [isInputEmpty, setIsInputEmpty] = useState(true);
+  const [outputVisible, setOutputVisible] = useState(false);
+  const [copiedMessageVisible, setCopiedMessageVisible] = useState(false); // State to track visibility of copied message
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     setError("");
-
-    console.log("Calling Mistral API...");
-    setIsLoading(true); // Set loading state to true when submitting form
+    setIsLoading(true);
 
     try {
       const response = await fetch("/api/rewrite", {
@@ -31,15 +34,35 @@ export default function Home() {
 
       const data = await response.json();
       const { output } = data;
-      // console.log(output)
-      console.log("Mistral replied: ", output);
-      setApiOutput(`${output}`);
+      setApiOutput(output);
+      setOutputVisible(true);
     } catch (error) {
-      console.error("Error from the CODE: ", error);
+      console.error("Error: ", error);
       setError("Failed to rewrite text. Please try again.");
     } finally {
-      setIsLoading(false); // Set loading state back to false when request is completed
+      setIsLoading(false);
     }
+  };
+
+  const clearInputText = () => {
+    setInputText("");
+    setIsInputEmpty(true);
+  };
+
+  const handleRewrite = () => {
+    setApiOutput("");
+    setOutputVisible(false);
+  };
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(apiOutput)
+      .then(() => {
+        setCopiedMessageVisible(true);
+        setTimeout(() => {
+          setCopiedMessageVisible(false);
+        }, 5000);
+      })
+      .catch((error) => console.error('Failed to copy:', error));
   };
 
   return (
@@ -64,13 +87,32 @@ export default function Home() {
               placeholder="start typing here"
               className="prompt-box"
               value={inputText}
-              onChange={(e) => setInputText(e.target.value)}
+              onChange={(e) => {
+                setInputText(e.target.value);
+                setIsInputEmpty(e.target.value === "");
+              }}
+              disabled={isLoading}
             />
+            {!isLoading &&
+              !isInputEmpty && (
+                <MdClear
+                  className="absolute top-3 right-3 text-white text-lg"
+                  onClick={clearInputText}
+                />
+              )}
           </div>
           <button
             type="submit"
             className="flex gap-1 items-center"
-            disabled={isLoading}
+            disabled={isLoading || isInputEmpty}
+            onClick={handleRewrite}
+            aria-label={
+              isLoading
+                ? "Result is being processed"
+                : isInputEmpty
+                ? "Input area is empty"
+                : ""
+            }
           >
             {isLoading ? (
               <div className="button-content">
@@ -89,24 +131,27 @@ export default function Home() {
               </div>
             )}
           </button>
-          {/* New code I added here */}
-          {apiOutput && (
-            <div className="output">
+          {outputVisible && (
+            <div>
               <div className="output-header-container">
                 <div className="output-header">
                   <h3>Output</h3>
                 </div>
               </div>
-              <div className="output-content">
-                <p>{apiOutput}</p>
+
+              <div className="relative">
+                <div className="output-content">{apiOutput}</div>
+                <BsClipboard2Fill
+                  className="absolute top-3 right-3 text-white text-xs cursor-pointer"
+                  onClick={copyToClipboard}
+                />
               </div>
             </div>
           )}
-          {/* <div className="output-content">
-          <p>{apiOutput}</p>
-        </div> */}
-          {error && <p style={{ color: "red" }}>{error}</p>}{" "}
-          {/* Displaying errors to the user */}
+          {copiedMessageVisible && (
+            <p className="copied-message">Content copied to clipboard</p>
+          )}
+          {error && <p style={{ color: "red" }}>{error}</p>}
         </form>
       </div>
     </div>
