521215cecdfcf3721b89442682d7bed51eb493fa
Harikrishna R <hk5983835@gmail.com>
2024-10-17 14:01:05 +0530

    update tailwind config to include more screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,13 @@ module.exports = {
   theme: {
     screens: {
       xs: "500px",
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
     },
+  
     extend: {},
   },
 };
